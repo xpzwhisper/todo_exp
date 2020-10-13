c5db9e5be3fdb96a711d7e6b89549f331ec5c76e
Oliver Roots <oliver.roots@gmail.com>
2020-10-13 20:59:43 +0300

    TodoList error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form.js';
+import TodoList from './components/TodoList.js';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
        <h1>Something interesting happening here!</h1>
        <Form />
+       <TodoList />
     </div>
   );
 }
